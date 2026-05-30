d7d96abe8ecec7ce80d95dc420033c35dc7040d7
c0d3n0te <doyoufollowme1012@gmail.com>
2026-05-30 15:57:45 -0500

    Update game.js

--- game.js
+++ game.js
@@ -39,7 +39,7 @@ function update() {
 
 function draw() {
   ctx.clearRect(0,0,canvas.width,canvas.height);
-  ctx.drawImage(pawn,x,y,40,50);
+  ctx.drawImage(pawn,x,y,50,60);
 }
 
 function loop() {
