96397df31c4e6475c3937929722e732f53ca5347
c0d3n0te <doyoufollowme1012@gmail.com>
2026-05-30 10:46:23 -0500

    Create game.js

---
+++ game.js
@@ -0,0 +1,31 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+const pawn = new Image();
+pawn.src = "pawn.png";
+
+let x = 50;
+let y = 50;
+
+function update() {
+  x +=1 ;
+}
+
+function draw() {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  ctx.drawImage(pawn,x,y,50,50);
+}
+
+function loop() {
+  update();
+  draw();
+  requestAnimationFrame(loop);
+}
+
+loop();
+
+document.addEventListener("keydown", e=> {
+  if (e.key === "ArrowRight") x += 5;
+  if (e.key === "ArrowLeft") x -= 5;
+  if (e.key === "ArrowUp") y -= 5;
+  if (e.key === "ArrowDown") y += 5;
+});
